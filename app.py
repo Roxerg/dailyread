0d5dc4c6bd10c5e32d6d6fdf2c2380c1cc80355e
Rokas G <roxguda@gmail.com>
2022-02-05 21:26:07 +0000

    prep for nginx

--- app.py
+++ app.py
@@ -151,7 +151,3 @@ async def home_page():
     return render_template("home.html", api_url=API_URL)
 
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
-
